19a756ec94e5f48e19c70f3463bc872ba25bfe9e
Obafemi Lawal <swissbobo@gmail.com>
2024-01-24 16:50:05 +0300

    enable autosave nd format, worked on program.cs file

--- Chapter01/Chapter01-vscode/AboutMyEnvironment/Program.cs
+++ Chapter01/Chapter01-vscode/AboutMyEnvironment/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine(Environment.OSVersion.VersionString);
+        Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
     }
 }
