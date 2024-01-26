60c40b7b5db85915158b23cbc590c41ede4cd53d
Obafemi Lawal <swissbobo@gmail.com>
2024-01-26 11:26:26 +0300

    done

--- Chapter01/Chapter01-vscode/AboutMyEnvironment/Program.cs
+++ Chapter01/Chapter01-vscode/AboutMyEnvironment/Program.cs
@@ -4,8 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine(Environment.CurrentDirectory);
         Console.WriteLine(Environment.OSVersion.VersionString);
         Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+
+        bool p = true;
+        bool q = false;
+        Console.WriteLine($"AND | p | q ");
+        Console.WriteLine($"p | {p & p,-5} | {p & q,-5} ");
+
+        int age = 50;
+        Console.WriteLine($"The {nameof(age)} variable uses {sizeof(int)} bytes of memory.");
     }
 }
--- Chapter01/HelloCS/HelloCS/Program.cs
+++ Chapter01/HelloCS/HelloCS/Program.cs
@@ -1,6 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
 Console.WriteLine("Hello, C#!");
 string name = typeof(Program).Namespace ?? "None!";
 Console.WriteLine($"Namespace: {name}");
-throw new Exception();
 
+var myApp = Assembly.GetEntryAssembly();
+
+if (myApp is null) return;
+// Loop through the assemblies that my app references.
+foreach (AssemblyName name1 in myApp.GetReferencedAssemblies())
+{
+    // Load the assembly so we can read its details.
+    Assembly a = Assembly.Load(name1);
+    // Declare a variable to count the number of methods.
+    int methodCount = 0;
+    // Loop through all the types in the assembly.
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        // Add up the counts of all the methods.
+        methodCount += t.GetMethods().Length;
+    }
+    // Output the count of types and their methods.
+    Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount,
+    arg2: name1.Name);
+}
