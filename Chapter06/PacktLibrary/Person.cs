fae3dccbb9b6c246df362fd9be8c837c542340b4
Obafemi Lawal <swissbobo@gmail.com>
2024-01-26 18:40:29 +0300

    Worked in person project

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,98 @@
+﻿namespace PacktLibrary;
+
+public class Person
+{
+    #region Properties;
+    public string? Name { get; set; }
+    public DateTimeOffset Born { get; set; }
+    public List<Person> Children = new();
+    public List<Person> Spouses = new();
+    public bool Married => Spouses.Count > 0;
+    #endregion
+
+    #region Methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {Born:dddd}.");
+    }
+    public void WriteChildrenToConsole()
+    {
+        string term = Children.Count == 1 ? "child" : "children";
+        WriteLine($"{Name} has {Children.Count} {term}.");
+    }
+
+    // Static method to marry two people.
+    public static void Marry(Person p1, Person p2)
+    {
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+        if (p1.Spouses.Contains(p2) || p2.Spouses.Contains(p1))
+        {
+            throw new ArgumentException(
+            string.Format("{0} is already married to {1}.",
+            arg0: p1.Name, arg1: p2.Name));
+        }
+        p1.Spouses.Add(p2);
+        p2.Spouses.Add(p1);
+    }
+
+    public static Person Procreate(Person p1, Person p2)
+    {
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+        if (!p1.Spouses.Contains(p2) && !p2.Spouses.Contains(p1))
+        {
+            throw new ArgumentException(string.Format(
+                "{0} must be married to {1} to procreate with them.", arg0: p1.Name, arg1: p2.Name));
+        }
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}",
+            Born = DateTimeOffset.Now
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+    #endregion
+
+    #region instance methods
+    // Instance method to marry another person.
+    public void Marry(Person partner)
+    {
+        Marry(this, partner); // "this" is the current person.
+    }
+
+    public void OutputSpouses()
+    {
+        if (Married)
+        {
+            string term = Spouses.Count == 1 ? "person" : "people";
+            WriteLine($"{Name} is married to {Spouses.Count} {term}:");
+            foreach (Person spouse in Spouses)
+            {
+                WriteLine($" {spouse.Name}");
+            }
+        }
+        else
+        {
+            WriteLine($"{Name} is a singleton.");
+        }
+    }
+
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+    #endregion
+
+    /// <summary>
+    /// Static method to "multiply" aka procreate and have a child together.
+    /// </summary>
+    /// <param name="p1">Parent 1</param>
+    /// <param name="p2">Parent 2</param>
+    /// <returns>A Person object that is the child of Parent 1 and Parent</returns>
+    /// <exception cref="ArgumentNullException">If p1 or p2 are null.</exception>
+    /// <exception cref="ArgumentException">If p1 and p2 are not married.</exception>
+    
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using PacktLibrary;
+Person harry = new()
+{
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25,
+ hour: 0, minute: 0, second: 0,
+ offset: TimeSpan.Zero)
+};
+harry.WriteToConsole();
+
+// Implementing functionality using methods.
+Person lamech = new() { Name = "Lamech" };
+Person adah = new() { Name = "Adah" };
+Person zillah = new() { Name = "Zillah" };
+// Call the instance method to marry Lamech and Adah.
+lamech.Marry(adah);
+// Call the static method to marry Lamech and Zillah.
+Person.Marry(lamech, zillah);
+lamech.OutputSpouses();
+adah.OutputSpouses();
+zillah.OutputSpouses();
+// Call the instance method to make a baby.
+Person baby1 = lamech.ProcreateWith(adah);
+baby1.Name = "Jabal";
+WriteLine($"{baby1.Name} was born on {baby1.Born}");
+// Call the static method to make a baby.
+Person baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tubalcain";
+adah.WriteChildrenToConsole();
+zillah.WriteChildrenToConsole();
+lamech.WriteChildrenToConsole();
+for (int i = 0; i < lamech.Children.Count; i++)
+{
+    WriteLine(format: " {0}'s child #{1} is named \"{2}\".",
+    arg0: lamech.Name, arg1: i, arg2: lamech.Children[i].Name);
+}
